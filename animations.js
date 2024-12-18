628bb2e0a85cbb4ffcb372bc1140de1dce6e16f9
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-12-18 20:56:20 +0100

    randomness

--- animations.js
+++ animations.js
@@ -13,3 +13,29 @@ function growDots() {
     dot.classList.add("growdot");
   });
 }
+
+function randShrink(range = 1000) {
+  const dots = document.querySelectorAll("div.dot");
+  const delay = [];
+  dots.forEach((dot) => {
+    dot.classList.add("shrinkdot");
+    dot.style.transitionDelay = getRandomInt(range).toString() + "ms";
+  });
+  dots.forEach((dot) => {
+    dot.style.width = "0";
+    dot.style.height = "0";
+  });
+}
+
+function randGrow(range = 1000) {
+  const dots = document.querySelectorAll("div.dot");
+  const delay = [];
+  dots.forEach((dot) => {
+    dot.style.animationDelay = getRandomInt(range).toString() + "ms";
+    dot.classList.add("growdot");
+  });
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
